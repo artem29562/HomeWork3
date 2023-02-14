04336b5c943c87beee84268d84d81c373bf259b8
artem29562 <artem29562@yandex.ru>
2023-02-14 12:30:22 +0500

    task1

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("task 1");
+        int a = 5;
+        byte b = 6;
+        short c = 7;
+        long d = 12L;
+        float e = 6.7F;
+        double f = 6.7877;
+
+        System.out.println("Значение переменной а с типом int равно " + a);
+        System.out.println("Значение переменной а с типом byte равно " + b);
+        System.out.println("Значение переменной а с типом short равно " + c);
+        System.out.println("Значение переменной а с типом long равно " + d);
+        System.out.println("Значение переменной а с типом float равно " + e);
+        System.out.println("Значение переменной а с типом double равно " + f);
+        System.out.println();
+
+    }
+}
